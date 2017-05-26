7cece22f452136546b4c1d2ec52d5a153662881c
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2017-05-26 06:31:01 +0200

    Add instanceof syntax tests

---
+++ src/test/resources/syntax/InstanceOf.java
@@ -0,0 +1,10 @@
+import semantics.model.Individual;
+
+public class Sample knows "music.rdf" {
+  public static void test() {
+    Individual ind = null;
+    boolean a = ind instanceof «:MusicArtist»;
+    boolean b = ind instanceof ∃«:influencedBy» ⇒ ⊤;
+    boolean c = new Object() instanceof ∀«a»⁻⁻ ⇒ «b» ⊔ «c» ⊓ «d»;
+  }
+}
