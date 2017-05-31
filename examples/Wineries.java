f0600499f8803d2da96cdceba8cd584cadb4813e
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2017-05-31 11:20:26 +0200

    Add wine makers example

---
+++ examples/Wineries.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Set;
+import semantics.model.Individual;
+import static semantics.Individuals.getNames;
+import static semantics.Individuals.getSorted;
+
+public class Wineries knows "wine.rdf" {
+  private static Set<«:Winery»> toMaker(Set<? extends Individual> wines) {
+    Set<«:Winery»> wineries = new HashSet<>();
+
+    for (Individual wine : wines) {
+      for (Individual maker : wine.«:hasMaker») {
+        wineries.add((«:Winery») maker);
+      }
+    }
+
+    return wineries;
+  }
+
+
+  public static void main(String[] args) {
+    Set<«:Winery»> makers = toMaker(do-query(":RedWine" ⊓ ":DryWine"));
+    System.out.println(String.join("\n", getNames(getSorted(makers))));
+  }
+}
