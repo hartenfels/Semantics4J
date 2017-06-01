ec4ffb065d88faa45073c4cc57ff70e85cf80d3b
Carsten Hartenfels <hartenfels@uni-koblenz.de>
2017-06-01 08:04:32 +0200

    Properly handle constant queries with parens

--- src/test/resources/integration/Unsatisfiable.java
+++ src/test/resources/integration/Unsatisfiable.java
@@ -5,9 +5,11 @@ import semantics.model.Individual;
 public class Unsatisfiable knows "music.rdf" {
   public static void main(String[] args) {
     Set<? extends Individual> set1 = do-query(⊥);
-    Set<? extends Individual> set2 = do-query(":MusicArtist" ⊓ ⊥);
-    Set<? extends Individual> set3 = do-query(":MusicArtist" ⊔ ⊥);
-    Set<? extends Individual> set4 = do-query(⊥ ⊓ ∃":influencedBy" ⇒ ⎨":beatles"⎬);
+    Set<? extends Individual> set2 = do-query((⊥));
+    Set<? extends Individual> set3 = do-query(":MusicArtist" ⊓ ⊥);
+    Set<? extends Individual> set4 = do-query(":MusicArtist" ⊔ ⊥);
+    Set<? extends Individual> set5 = do-query(⊥ ⊓ ∃":influencedBy" ⇒ ⎨":beatles"⎬);
+    Set<? extends Individual> set6 = do-query((⊥) ⊓ (∃(":influencedBy") ⇒ (⎨":beatles"⎬)));
   }
 }
 
@@ -15,5 +17,7 @@ public class Unsatisfiable knows "music.rdf" {
 /*
  *! src/test/resources/integration/Unsatisfiable.java:7: error: constant query is unsatisfiable
  *! src/test/resources/integration/Unsatisfiable.java:8: error: constant query is unsatisfiable
- *! src/test/resources/integration/Unsatisfiable.java:10: error: constant query is unsatisfiable
+ *! src/test/resources/integration/Unsatisfiable.java:9: error: constant query is unsatisfiable
+ *! src/test/resources/integration/Unsatisfiable.java:11: error: constant query is unsatisfiable
+ *! src/test/resources/integration/Unsatisfiable.java:12: error: constant query is unsatisfiable
  */
